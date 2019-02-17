18fee6c13310e3ef38bf670c5c9d7638d05234d2
Robert Bergers <bergersr@my.easternct.edu>
2019-02-17 13:39:31 -0500

    Set imports  Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   main/app.py
    	new file:   readme.md

---
+++ main/app.py
@@ -0,0 +1,24 @@
+
+"""
+Author: Robby Bergers
+Info: Gather media to be displayed on webpage
+"""
+
+import requests
+from urllib import request
+import os
+from time import sleep
+import logging
+import json
+import pytube
+from bs4 import BeautifulSoup
+
+def getInfo():
+
+
+
+
+
+
+if __name__ == '__main__':
+	getInfo()
