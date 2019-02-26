d807a04cf887eac2866e58243884e40fdf793f31
Robert Bergers <bergersr@my.easternct.edu>
2019-02-26 17:41:56 -0500

    Add styles, add event listener function for nav-overlay  Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is ahead of 'origin/master' by 4 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   docs/assets/css/default.css
    	modified:   docs/assets/css/default.scss
    	new file:   docs/assets/images/favicon.png
    	modified:   docs/assets/js/default.js
    	modified:   docs/assets/templates/header.html
    	modified:   docs/assets/templates/topbar.html
    	modified:   docs/index.html

--- docs/assets/js/default.js
+++ docs/assets/js/default.js
@@ -5,3 +5,26 @@
   This is the main JS file used by the BADMedia page
 
 */
+
+/* Variables */
+
+navbar = false;
+
+/* Functions */
+
+$(document).ready(function() {
+
+  // Set event listeners
+  $("#navbar").click(function() {
+    toggleNav();
+  });
+
+});
+
+function toggleNav() {
+  if (navbar) {
+    $("#nav").css("height", "0");
+  } else {
+    $("#nav").css("height", "100vh");
+  }
+}
