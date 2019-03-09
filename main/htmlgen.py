0257e1f9e335cb8fff3021f4ce69935669cbbee2
Robert Bergers <bergersr@my.easternct.edu>
2019-03-09 09:59:56 -0500

    set logger, add imports

--- main/htmlgen.py
+++ main/htmlgen.py
@@ -9,6 +9,27 @@
 import os
 import app
 import jsonhandler
+import json
+import logging
+
+# Base Directory
+appdir = os.path.dirname(os.path.realpath(__file__))
+jsondir = '%s/json/' % appdir
+webdir = '%s/../docs/' % appdir
+
+# Set Logger
+log = logging.getLogger(__name__)
+log.setLevel(logging.WARNING)
+handlerpath = appdir + '/app.log'
+handler = logging.FileHandler(handlerpath)
+handler.setLevel(logging.DEBUG)
+consoleHandler = logging.StreamHandler()
+formatter = logging.Formatter('[%(levelname)s]: %(message)s')
+handler.setFormatter(formatter)
+consoleHandler.setFormatter(formatter)
+log.addHandler(consoleHandler)
+log.addHandler(handler)
+log.info('Running file ~/main/htmlgen.py:')
 
 """ Make blog sections """
 # (source = json object)
--- main/jsonhandler.py
+++ main/jsonhandler.py
@@ -9,7 +9,28 @@
 
 import app
 import htmlgen
+import os
+import json
+import logging
 
+# Base Directory
+appdir = os.path.dirname(os.path.realpath(__file__))
+jsondir = '%s/json/' % appdir
+webdir = '%s/../docs/' % appdir
+
+# Set Logger
+log = logging.getLogger(__name__)
+log.setLevel(logging.WARNING)
+handlerpath = appdir + '/app.log'
+handler = logging.FileHandler(handlerpath)
+handler.setLevel(logging.DEBUG)
+consoleHandler = logging.StreamHandler()
+formatter = logging.Formatter('[%(levelname)s]: %(message)s')
+handler.setFormatter(formatter)
+consoleHandler.setFormatter(formatter)
+log.addHandler(consoleHandler)
+log.addHandler(handler)
+log.info('Running file ~/main/jsonhandler.py:')
 
 """ Ensure jsonlist matches JSON objects """
 def cleanJSONList():
