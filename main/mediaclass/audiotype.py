63fdbb33a3bcdcb69069dcc794ff7b97ff1658c3
Robert Bergers <bergersr@my.easternct.edu>
2019-03-11 13:42:25 -0400

    create getInfo function for audio class

--- main/mediaclass/audiotype.py
+++ main/mediaclass/audiotype.py
@@ -19,16 +19,29 @@ log = logging.getLogger('BADMedia')
 class Audio:
 
     def __init__(self, dir, filename = None):
+        # Set base project directories
         self.appdir = dir
         self.jsondir = '%s/json/' % dir
         self.webdir = '%s/../docs/' % dir
+
         if filename:    # If reference to json file exists
             self.jsonobject = json.loads('%s%s.json' % (jsondir, filename))
             self.name = self.jsonobject['name']
+            self.link = self.jsonobject['xml']
             self.media = self.jsonobject['media']
+            self.tag = self.jsonobject['tag']
         else:       # If this is a new media object
-            self.createJSON()
+            self.getInfo()
+        return
 
-    # Get information from user to create JSON object and file
-    def createJSON(self):
-        self.name = str(input('What should this media be called?'))
+    def getInfo(self):
+        # Get object information from user
+        self.type = 'audio'
+        self.name = str(input('What should this media be called?\n'))
+        self.link = str(input('Enter the link to the feed:\n'))
+        self.tag = str(input('Enter tag name containing media: [default = \'enclosure\']'))
+        if not self.tag:
+            self.tag = 'enclosure'
+
+        self.saveToJSON()
+        return
