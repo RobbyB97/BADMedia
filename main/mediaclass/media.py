19438ae30875c854116ff67b17fc6d14dfc36640
Robert Bergers <bergersr@my.easternct.edu>
2019-03-12 08:52:41 -0400

    set init method for media class

--- main/mediaclass/media.py
+++ main/mediaclass/media.py
@@ -18,3 +18,16 @@ import audiotype
 import imagetype
 import texttype
 import youtubetype
+
+# Set Logger
+log = logging.getLogger('BADMedia')
+
+
+
+class Media:
+
+
+    def __init__(self, dir, filename = None):
+        log.info('New Media class initializing...')
+
+        return
