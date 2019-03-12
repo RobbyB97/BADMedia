b2c12743d9daf1667f7fdb2342c02a981012e3a4
Robert Bergers <bergersr@my.easternct.edu>
2019-03-12 07:47:02 -0400

    turn main python file into BADMedia class

--- main/app.py
+++ main/app.py
@@ -2,8 +2,7 @@
 
     Author: Robby Bergers
 
-    This is the main file for gathering media, constructing JSON objects
-    from it and downloading media locally
+    This is the main file containing the BADMedia class
 
 """
 
@@ -15,11 +14,6 @@ from time import sleep
 import logging
 import json
 from bs4 import BeautifulSoup as bs
-
-# Base Directory
-appdir = os.path.dirname(os.path.realpath(__file__))
-jsondir = '%s/json/' % appdir
-webdir = '%s/../docs/' % appdir
 
 # Internal Libraries
 import htmlgen
@@ -33,7 +27,7 @@ from mediaclass import texttype
 # Set Logger
 log = logging.getLogger('BADMedia')
 log.setLevel(logging.WARNING)
-handlerpath = appdir + '/app.log'
+handlerpath = os.path.dirname(os.path.realpath(__file__)) + '/app.log'
 handler = logging.FileHandler(handlerpath)
 handler.setLevel(logging.DEBUG)
 consoleHandler = logging.StreamHandler()
@@ -44,88 +38,75 @@ log.addHandler(consoleHandler)
 log.addHandler(handler)
 log.info('Running file ~/main/app.py:')
 
-# Variables for storing classes
-audios = []         # Audio media object instances appended here
-youtubes = []       # Youtube media object instances appended here
-images = []         # Image media object instances appended here
-texts = []          # Text media object instances appended here
-htmler = html.Writer(dir=appdir)  # Object that generates HTML file
 
 
-
-""" Take user input to decide what to do """
-def mainMenu():
-
-    log.info('mainMenu started:')
-    os.chdir(appdir)
-
-    inp = str(input('What would you like to do?\n\
-    add: add new media source\n\
-    clear: remove a media source\n\
-    clearall: clear all media sources\n\
-    create: generate webpage\n\
-    exit: exit.\n'))
-    if inp == 'add':
-        jso.getInfo()
-    elif inp == 'clear':
-        source = str(input('Which source do you want to remove?'))
-        jso.clearJSON(source)
-    elif inp == 'clearall':
-        jso.clearAllJSON()
-    elif inp == 'create':
-        htmlgen.createPage()
-    elif inp == 'exit':
-        return
-    else:
-        print('Input invalid!')
+class BADMedia:
 
 
+    def __init__(self):
+        log.info('New BADMedia class initializing...')
 
-""" Menu for class version """
-def menu():
-    log.info('mainMenu started:')
-    os.chdir(appdir)
+        # Base Directory
+        self.appdir = os.path.dirname(os.path.realpath(__file__))
+        self.jsondir = '%s/json/' % self.appdir
+        self.webdir = '%s/../docs/' % self.appdir
 
-    inp = str(input('What would you like to do?\n\
-    add: add new media source\n\
-    clear: remove a media source\n\
-    clearall: clear all media sources\n\
-    create: generate webpage\n\
-    exit: exit.\n'))
-    if inp == 'add':
-        answer = str(input('What type of media?\n\
-        audio: an podcast or other audio stream\n\
-        youtube: a youtube channel\n\
-        image: an image feed\n\
-        text: a text article feed\n\
-        back: back to menu.\n'))
-        if answer == 'audio':
-            media = audiotype.Audio(dir=appdir)
-            audios.append(media)
-        elif answer == 'youtube':
-            media = youtubetype.YouTube()
-            youtubes.append(media)
-        elif answer == 'image':
-            media = imagetype.Image()
-            images.append(media)
-        elif answer == 'text':
-            media = texttype.Text()
-            texts.append(media)
-        elif answer == 'back':
-            menu()
+        # Create class lists / html writer class instance
+        self.htmls = html.Writer(dir=self.appdir)
+        self.audios = []
+        self.youtubes = []
+        self.images = []
+        self.texts = []
+
+
+    def menu(self):
+        log.info('menu started:')
+        os.chdir(self.appdir)
+
+        inp = str(input('What would you like to do?\n\
+        add: add new media source\n\
+        clear: remove a media source\n\
+        clearall: clear all media sources\n\
+        create: generate webpage\n\
+        exit: exit.\n'))
+
+        if inp == 'add':
+            answer = str(input('What type of media?\n\
+            audio: an podcast or other audio stream\n\
+            youtube: a youtube channel\n\
+            image: an image feed\n\
+            text: a text article feed\n\
+            back: back to menu.\n'))
+            if answer == 'audio':
+                media = audiotype.Audio(dir=self.appdir)
+                self.audios.append(media)
+            elif answer == 'youtube':
+                media = youtubetype.YouTube(dir=self.appdir)
+                self.youtubes.append(media)
+            elif answer == 'image':
+                media = imagetype.Image(dir=self.appdir)
+                self.images.append(media)
+            elif answer == 'text':
+                media = texttype.Text(dir=self.appdir)
+                self.texts.append(media)
+            elif answer == 'back':
+                self.menu()
+            else:
+                print('Input invalid!')
+        #TODO: Add clear option
+        #TODO: Add clearall option
+        #TODO: Add create option
         else:
             print('Input invalid!')
-    else:
-        print('Input invalid!')
 
-""" Main Process """
+
+
 if __name__ == '__main__':
-
     log.info('app.py started:')
-    os.chdir(appdir)
 
     try:
-        menu()
+        bad = BADMedia()
+        bad.menu()
 
     except Exception:
         log.exception('Error in main process')
