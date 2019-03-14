d507d0abe9994e0d851e5b488a1cb4794b268950
Robert Bergers <bergersr@my.easternct.edu>
2019-03-14 07:11:06 -0400

    add getWraps function to writer class, run on init

--- main/htmlclass/htmlwriter.py
+++ main/htmlclass/htmlwriter.py
@@ -2,7 +2,7 @@
 
     Author: Robby Bergers
 
-    This class is responsible for gathering instances of other media 
+    This class is responsible for gathering instances of other media
     classes and generating a webpage from them.
 
 """
@@ -21,11 +21,15 @@ class Writer:
 
     def __init__(self, dir):
         log.debug('Initializing Writer class...')
-        
+
         # Load base project directories
         self.appdir = dir
         self.jsondir = '%s/json/' % dir
         self.webdir = '%s/../docs/' % dir
+
+        # Methods to run on init
+        self.loadMedia()
+        self.getWraps()
         return
 
 
@@ -41,6 +45,24 @@ class Writer:
     	    self.text = texts
         if images:
     	    self.image = images
+        return
+
+
+    def getWraps(self):
+        log.debug('Writer.getWraps started...')
+
+        # Wrapper dictionary and keys
+        self.mediawraps = {}
+        types = ['audio', 'text', 'image', 'youtube']
+
+        # Get wrap for each media type and add to dictionary
+        for type in types:
+            try:        # Open type wrap template, if it exists
+                os.chdir(self.webdir)
+                with open('./assets/templates/%s/wrap.html' % type, 'r') as f:
+                    self.mediawraps[type] = f.read().split('|')
+            except:
+                log.exception('Could not find %s wrap template...' % type)
         return
 
 
