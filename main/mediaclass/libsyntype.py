e6ab1cb69b364069c519ee2470b052208b4218da
Robert Bergers <bergersr@my.easternct.edu>
2019-03-30 18:30:01 -0400

    init and getinfo functions

--- main/mediaclass/libsyntype.py
+++ main/mediaclass/libsyntype.py
@@ -15,17 +15,45 @@ import requests
 import time
 from libpyn.podcast import Podcast
 
-class Libsyn:
+class Libsyn(Media):
 
 
-    def __init__(self):
+    def __init__(self, dir, filename=None):
 
+        # Set attributes
         self.type = 'libsyn'
+        self.dir = dir
 
+        # Load JSON object (if filename argument passed)
+        if filename:
+            try:
+                os.chdir(self.dir['json'])
+                with open(filename, 'r') as f:
+                    jsonobject = json.loads(f.read())
+            except:
+                log.warning('Error loading %s.json. Creating new media source...' % filename)
+                self.getInfo()
+
+            # Assign class attributes from JSON object
+            self.name = jsonobject['name']
+            self.link = jsonobject['xml']
+            self.media = jsonobject['media']
+
+        else:
+            self.getInfo()
         return
 
 
     def getInfo(self):
+        log.debug('Libsyn.getInfo started...')
+
+        # Get link from user, initialize as Podcast class
+        self.link = str(input('Enter the link to the podcast:'))
+
+        self.podcast = Podcast(self.link)
+        self.name = self.podcast.name
+
+        self.updateJSON()
         return
 
 
